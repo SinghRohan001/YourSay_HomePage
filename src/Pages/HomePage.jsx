1d185853193ff13d37c771df0acc10ea8325c92d
Pundir9711 <himanshu@asargaming.com>
2024-12-03 12:18:08 +0530

    Circle Changes Done

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -13,14 +13,14 @@ const HomePage = () => {
   return (
     <div className="w-full">
       <Navbar />
-      <div className="lg:mb-14 md:mb-52">
+      <div className="lg:mb-14 w-full md:mb-52">
         <div className="relative">
           <div className="flex justify-center items-center">
             <div className="absolute lg:mt-20 mt-32 animate-[rotateAlternating_25s_linear_infinite] z-0 hidden md:block lg:block">
               <img
                 src={rotate1}
                 alt="Rotating background 3"
-                className="lg:w-[100%] md:w-[96vw]"
+                className="lg:max-w-[1416px] md:max-w-[764px]"
               />
             </div>
 
@@ -28,7 +28,7 @@ const HomePage = () => {
               <img
                 src={rotate3}
                 alt="Rotating background 2"
-                className="lg:w-[100%] md:w-[80vw]"
+                className="lg:max-w-[1120px] md:max-w-[620px]"
               />
             </div>
 
@@ -36,18 +36,18 @@ const HomePage = () => {
               <img
                 src={rotate2}
                 alt="Rotating background 1"
-                className="lg:w-[100%] md:w-[62vw]"
+                className="lg:max-w-[810px] md:max-w-[460px]"
               />
             </div>
           </div>
         </div>
 
-        <div className="flex relative justify-center items-start lg:h-screen mb-6">
-          <div className="w-[100%] lg:w-[35%] md:w-[50%] mt-16 lg:mt-28 md:mt-20 flex flex-col justify-center">
-            <h1 className="lg:text-5xl md:text-xl text-3xl lg:mt-0 md:mt-0 text-center font-montserrat font-[700] tracking-wider">
+        <div className="flex relative justify-center w-full items-start lg:h-screen mb-6">
+          <div className="w-[100%] lg:w-[35%] md:w-[50%] mt-16  lg:mt-28 md:mt-20 flex flex-col justify-center">
+            <h1 className="lg:text-5xl  md:text-lg w-full text-3xl lg:mt-0 md:mt-0  text-center font-montserrat font-[700] tracking-wider">
               Turn Your Opinions into Real Rewards!
             </h1>
-            <p className="text-center text-gray-500 lg:mt-6 md:mt-2 mt-4 lg:text-[14px] md:text-xs text-sm lg:p-0 md:p-0 px-2 font-montserrat font-[400]">
+            <p className="text-center text-gray-500 lg:mt-6 md:mt-2 mt-4 lg:text-[14px] md:text-[8px] text-sm lg:px-10 md:px-16 px-2 font-montserrat font-[400]">
               Welcome to YourSay, the platform where your opinions take center
               stage. Here, every 'yes' or 'no' is more than just a choice—it's
               an opportunity to win real rewards. Engage in exciting opinion
